d5f0ee2895d48d5b524b63e2114a264beed43f01
Angus Goldsmith <durins_beard@hotmail.com>
2017-06-17 21:14:12 +0100

    Upgraded to work with Spring Boot 2.

--- src/main/java/com/auth0/spring/security/mvc/Auth0Config.java
+++ src/main/java/com/auth0/spring/security/mvc/Auth0Config.java
@@ -5,7 +5,7 @@ import com.auth0.jwt.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.filter.OrderedRequestContextFilter;
+import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
